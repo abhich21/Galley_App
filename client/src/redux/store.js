09b6e384335fdd56031441833a63fb77015301cc
Abhishek Choudhary <96258754+abhich21@users.noreply.github.com>
2022-10-22 03:33:09 +0530

    Add files via upload

---
+++ client/src/redux/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import gallerySlice from "./reducers/gallerySlice";
+
+
+const store = configureStore({
+    reducer: {
+        gallery: gallerySlice
+    },
+});
+
+export default store;
